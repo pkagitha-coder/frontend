36a085e1cb85db80e4cd0876fba8711a8ab413a0
Pradeep Kagithalagudem <gallant.pradeep@gmail.com>
2020-03-15 22:47:11 +0530

    User list component

---
+++ src/user/components/UsersList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import UserItem from "./UserItem";
+import "./UsersList.css";
+
+const UsersList = props => {
+  if (props.items.length === 0) {
+    return (
+      <div class="center">
+        <h2>No Users found</h2>
+      </div>
+    );
+  }
+  return (
+    <ul className="users-list">
+      {props.items.map(user => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          name={user.name}
+          image={user.image}
+          places={user.places}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default UsersList;
---
+++ src/user/pages/Users.js
@@ -0,0 +1,17 @@
+import React from "react";
+import UsersList from "../components/UsersList";
+
+const Users = () => {
+  const USERS = [
+    {
+      id: 1,
+      name: "Alex",
+      image:
+        "https://www.goodmorningimagesdownload.com/wp-content/uploads/2019/10/Nice-Whatsapp-Dp-Profile-Images-101-300x300.jpg",
+      places: 3
+    }
+  ];
+  return <UsersList items={USERS} />;
+};
+
+export default Users;
